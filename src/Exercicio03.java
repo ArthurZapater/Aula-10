04af470867d229774d5f3ed7b996974f41180c8a
Arthur Zapater <arthur.zapater@gmail.com>
2025-08-18 22:04:28 -0300

    "Atualizando Exercicio03"

--- src/Exercicio03.java
+++ src/Exercicio03.java
@@ -3,25 +3,26 @@ import java.util.Scanner;
 public class Exercicio03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double[] media = new double[10];
+        double[] media = new double[5];
         int numAlunos = 0;
 
         for (int i = 0; i < media.length; i++) {
-            double total = 0;
+            double notas = 0;
 
             System.out.print("Digite as 4 notas do " + (i+1) + "º" + " aluno \n");
 
-            for (int j = 0; j < 4; j++) {
-                System.out.print("Digite a " + (j+1) + "º" +  " nota --> ");
-                total += sc.nextDouble();
+            for (int j = 1; j <= 4; j++) {
+                System.out.print("Digite a " + (j) + "º" +  " nota --> ");
+                notas += sc.nextDouble();
             }
 
-                media[i] = total / 4.0;
+                media[i] = notas / 4.0;
 
                 if (media[i] >= 6) {
                     numAlunos++;
                 }
 
+                System.out.println("==========");
         }
         System.out.println("Numeros de alunos com média maior ou igual a 6,0 -->  " + numAlunos);
         sc.close();
